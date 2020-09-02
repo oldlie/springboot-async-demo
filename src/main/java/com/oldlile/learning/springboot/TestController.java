80d3dd8efc2f7e2b2809ac6c057de86d7c1d2b77
oldlie <oldlie@github.com>
2020-09-02 14:07:33 +0800

    添加了validation测试

--- src/main/java/com/oldlile/learning/springboot/TestController.java
+++ src/main/java/com/oldlile/learning/springboot/TestController.java
@@ -1,14 +1,20 @@
 package com.oldlile.learning.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Size;
 
 /**
  * @author CL
  * @date 2020/8/31
  */
 @RestController
+@Validated
 public class TestController {
 
     @Autowired
@@ -21,5 +27,10 @@ public class TestController {
         asyncTask.task2();
         asyncTask.task3();
         return (System.currentTimeMillis() - start) + "ms";
+    }
+
+    @RequestMapping("/validation")
+    public String validation(@Size(max = 2, message = "max size 2") @RequestParam("t") String title) throws Exception {
+        return title;
     }
 }
